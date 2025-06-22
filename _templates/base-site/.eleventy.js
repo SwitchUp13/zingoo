2928866c54bf4cb4bd3a435c47fa5343a8f8c917
SwitchUp13 <jack.hamilton.gibbs@gmail.com>
2025-06-22 18:10:18 -0500

    luxon add

--- _templates/base-site/.eleventy.js
+++ _templates/base-site/.eleventy.js
@@ -1,4 +1,12 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Add a Nunjucks date filter
+  eleventyConfig.addFilter("date", (value, format = "yyyy") => {
+    return DateTime.fromJSDate(new Date(value)).toFormat(format);
+  });
+
+  // Pass through static assets like styles
   eleventyConfig.addPassthroughCopy("styles");
 
   return {
